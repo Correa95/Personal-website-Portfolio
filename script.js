e29f56f7aecaa41c6c610b7c7609872cc888adce
Correa95 <mathewcorrea19@gmail.com>
2024-07-28 21:44:02 -0400

    Completed personal Portfolio

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World!");
+
+// Contact Form Submit
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+// Form EventListener action
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
+
+// Settimeout  function
+setTimeout(() => {
+  window.location.href = window.location.origin;
+}, 5000);
